2018f93e7376b6efa10f7e672048508333bc5edb
Vinicius Gabriel <96842730+ViniciusGabriel0@users.noreply.github.com>
2023-03-16 16:12:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function entrar() {
+    let login = document.getElementById('login').value
+    let senha = document.getElementById('senha').value
+
+    if (login == "" && senha == "") {
+      alert('Preencha os campos abaixo.')
+    } 
+
+}
